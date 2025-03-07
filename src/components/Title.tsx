f64c383e95cb7b426686710a4477b4ee3b716bc8
Mohit Shahi <moheetshahi92@gmail.com>
2025-03-07 13:31:31 +0545

    added github link

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,8 +2,17 @@ import React from "react"
 
 const Title:React.FC = function(){
     return (
-      <h1 className="bg-primary text-green-600 text-3xl text-center py-3 uppercase">CPU Simulator</h1>
-    )
+      <div className="flex items-center justify-center bg-primary flex-col">
+        <h1 className=" text-green-600 text-3xl text-center pt-3 uppercase">
+          CPU Simulator
+        </h1>
+        <div>
+          <a href="https://github.com/mohitShahiT/CPU-simulator" target="blank">
+            <img className="h-7 mt-1" src="/github.png"></img>
+          </a>
+        </div>
+      </div>
+    );
 
 }
 
